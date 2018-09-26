f1eaba2093361e84fa002e3817f7af11640c110c
Manal Liaquat <m.manalliaquat@gmail.com>
2018-09-26 20:06:32 +0500

    Judges remarks the student by giving 5 stars

--- src/Screens/Judges/Judges.js
+++ src/Screens/Judges/Judges.js
@@ -1,38 +1,44 @@
 import React from "react";
 
 export default class Judge extends React.Component {
-  constructor() {
-    super()
-    this.state = { stars: 0, available: false }
+  constructor(props) {
+    super(props)
+    this.state = { stars: 0, available: props.available }
     this.applaud = this.applaud.bind(this)
     this.provideStars = this.provideStars.bind(this)
   }
 
   applaud() {
-    //Send this applaud status to Kid.js
     this.props.getApplaudStatus(true)
   }
 
   provideStars() {
     const { stars } = this.state;
+    this.setState({
+      stars: stars + 1
+    })
+    this.props.recieveStars(stars + 1)
+  }
 
+  shouldComponentUpdate() {
+    const { stars } = this.state;
+
+    return stars < 5 ? true : false
   }
 
   render() {
     const { stars, available } = this.state;
+    // console.log(stars, "render");
 
     return (
       <div>
-        <button type="button" onClick={this.applaud}>
-          Appreciate performance
-          </button>
-        <button type="button" onClick={this.provideStars}>
-          Provide stars
-          </button>
-
-        Kid is available: {available}
-
+        <button type="button" onClick={this.applaud}>Appreciate performance</button>
+        <button type="button" onClick={this.provideStars}>Provide stars</button>
+        <br />
+        Kid is available: {available ? "yes" : 'no'}
+        <br />
         Stars gained: {stars}
+        <br />
       </div>
     );
   }
