42969154fbaf1db9f67eabe428b54c96e02836a4
Manal Liaquat <m.manalliaquat@gmail.com>
2018-09-26 20:05:51 +0500

    Kid is performing dance on the stage

--- src/Screens/Kid/Kid.js
+++ src/Screens/Kid/Kid.js
@@ -4,14 +4,13 @@ export default class Kid extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { emotion: 'nervous', danceSteps: [], currentStepIndex: 0, startedPerforming: false, applaudStr: false };
+    this.state = { emotion: 'nervous', danceSteps: [], currentStepIndex: 0, startedPerforming: false };
   }
 
   static getDerivedStateFromProps(props, state) {
     const { furtherSteps, sendApplaudStatus } = props
     // console.log(state.danceSteps)
-    console.log(props);
-
+    // console.log(props);
 
     return { danceSteps: furtherSteps, emotion: sendApplaudStatus ? 'Happy' : 'nervous' }
   }
@@ -23,20 +22,29 @@ export default class Kid extends React.Component {
     this.setState({ danceSteps, startedPerforming: true })
   }
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.sendStars === 4) {
+      this.qualified()
+    }
+  }
 
   qualified() {
     this.setState({ startedPerforming: false })
   }
 
+  componentWillUnmount() {
+    this.props.renderJudgesComp(false)
+  }
+
   render() {
     const { dressColor } = this.props;
     const { danceSteps, emotion, startedPerforming, currentStepIndex } = this.state;
-    console.log(danceSteps, currentStepIndex, danceSteps[currentStepIndex], "***");
-    console.log(this.state.applaudStr);
+    // console.log(danceSteps, currentStepIndex, danceSteps[currentStepIndex], "***");
     return (
       <div>
         <div>dressColor: {dressColor})</div>
         <div style={{ backgroundColor: dressColor, width: 50, height: 50 }}></div>
+        <div>Performing: {startedPerforming ? 'yes' : 'no'}</div>
         <div>Emotion: {emotion})</div>
         {startedPerforming && <div>
           Current Step: {danceSteps[currentStepIndex]})
